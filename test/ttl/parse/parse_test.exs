86628c2d21e86372b34a00aa8330461f5fb207d6
TJ <tj@tj.com>
2017-08-24 20:48:24 -0700

    tests - added some import/export tests

--- test/ttl/parse/parse_test.exs
+++ test/ttl/parse/parse_test.exs
@@ -4,10 +4,23 @@ defmodule Ttl.ParseTest do
   alias Ttl.Parse.Import
   alias Ttl.Parse.Export
 
-  describe "headline" do
+  # TODO - tags and multiple scheduled dates are failing
+  describe "test complex file" do
     test "import notes" do
-      {:ok, doc, objects} = Ttl.Parse.Import.import_file("test/ttl/fixtures/notes.org")
-      :ok = Ttl.Parse.Export.export_file("/tmp/notes.org", doc.id, false)
+      {:ok, doc, objects} = Ttl.Parse.Import.import_file("test/ttl/fixtures/test.org")
+      :ok = Ttl.Parse.Export.export_file("/tmp/test.org", doc.id, false)
+      {diff, ret} = System.cmd("diff", ["-w" ,"test/ttl/fixtures/test.org", "/tmp/test.org"])
+      assert diff == ""
+      assert ret == 0
+    end
+  end
+  describe "test README" do
+    test "import readme" do
+      {:ok, doc, objects} = Ttl.Parse.Import.import_file("README.org")
+      :ok = Ttl.Parse.Export.export_file("/tmp/README.org", doc.id, false)
+      {diff, ret} = System.cmd("diff", ["-w" ,"README.org", "/tmp/README.org"])
+      assert diff == ""
+      assert ret == 0
     end
   end
 end

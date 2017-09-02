a05d46aec1bf770538a7312cf3a68a7693a013e7
TJ <tj@tj.com>
2017-09-01 21:06:24 -0700

    auth - adding some resource controls for not logged in

--- lib/ttl/accounts/plug_auth.ex
+++ lib/ttl/accounts/plug_auth.ex
@@ -11,6 +11,20 @@ defmodule Ttl.Accounts.PlugAuth do
       nil -> assign(conn, :current_user, nil)
       _ -> conn |> configure_session(renew: true)
     end
+    resource_auth(conn)
+  end
+
+  def resource_auth(conn) do
+    case conn.private.plug_session["current_user"]  do
+      nil ->
+        if conn.request_path in ["/"] or String.starts_with?(conn.request_path, "/sessions") do
+          conn
+        else
+          halt_forbidden(conn, "Need to be logged in")
+        end
+      _ ->
+        conn
+    end
   end
 
   # TODO:
@@ -25,5 +39,11 @@ defmodule Ttl.Accounts.PlugAuth do
 
   def logout(conn) do
     conn |> configure_session(drop: true)
+  end
+
+  def halt_forbidden(conn, error) do
+    conn
+    |> send_resp(403, error)
+    |> halt
   end
 end
